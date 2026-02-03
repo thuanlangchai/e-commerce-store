798858144790f875f6b91fab8d3be227e616b195
thuanlangchai <thuan08022006@gmail.com>
2026-02-03 21:48:31 +0700

    add new repository

---
+++ src/main/java/com/minhthuan/web_store/WebStoreApplication.java
@@ -0,0 +1,13 @@
+package com.minhthuan.web_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/minhthuan/web_store/WebStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minhthuan.web_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
